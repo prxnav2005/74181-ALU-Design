68414e7ff772f73053ad6185f4e1cceae14e0372
prxnav2005 <pranavmofficial2005@gmail.com>
2024-10-26 20:57:34 +0530

    added CLA

---
+++ testbench/CLA_tb.sv
@@ -0,0 +1,23 @@
+module testbench;
+  reg [3:0] A, B;
+  reg Cin;
+  wire [3:0] G, P, C, S;
+  wire Cout;
+  
+  four_bit_cla DUT (A, B, Cin, G, P, C, S, Cout);
+
+  initial begin
+    A = 4'b0000; B = 4'b0000; Cin = 0;
+    #10;
+    A = 4'b0101; B = 4'b0011; Cin = 1;
+    #10;
+    A = 4'b1111; B = 4'b0001; Cin = 0;
+    #10;
+    A = 4'b1001; B = 4'b0110; Cin = 1;
+    #10;
+    A = 4'b1010; B = 4'b1101; Cin = 0;
+    #10;
+
+    $finish;
+  end
+endmodule
